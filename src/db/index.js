6943d5d180baee8d20d2b40c8905cc8f6c7c9fc8
Chayuan <gwenael.rob@gmail.com>
2019-12-10 22:16:26 +0100

    backup vendredi 6/12/2019

---
+++ src/db/index.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import { resolveSoa } from 'dns'
+
+const connect = () => {
+  mongoose.connect('mongodb://localhost:27017/yuka-miw', {
+    autoReconnect: true,
+    useNewUrlParser: true
+  })
+}
+
+export let db
+
+export default () => {
+  return new Promise((resolve, reject) => {
+    db = mongoose.connection
+
+    db.on('connecting', () => {
+      console.log('Connexion à Mongo.... ')
+    })
+
+    db.on('error', err => {
+      mongoose.disconnect()
+      reject(err)
+      throw new Error(err)
+    })
+
+    db.once('open', () => {
+      console.log('Connecté à Mongo !')
+      resolve()
+    })
+
+    db.on('disconnected', () => {
+      setTimeout(() => {
+        try {
+          connect()
+        } catch (err) {
+          throw new Error('Cannot reconnect')
+        }
+      }, 5000)
+    })
+
+    connect()
+  })
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import routes from './routes'
+import InitDb from './db'
+
+export default async () => {
+  try {
+    const app = express()
+
+    await InitDb()
+    routes(app)
+
+    app.listen(process.env.PORT, () => {
+      console.log(process.env.PORT, process.env.NODE_ENV)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
